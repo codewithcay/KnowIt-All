26b9f7333694dfc8e5d93ccab399614c6b3776b7
Cay <162811181+codewithcay@users.noreply.github.com>
2026-01-29 11:13:14 +0100

    Initialize session and fetch categories from database

--- index.php
+++ index.php
@@ -7,10 +7,35 @@
     <link rel="stylesheet" href="stylesheets/landingpage.css">
 </head>
   <body>
+    <?php
+    session_start();
+    $_SESSION['score'] = 0;
+    $_SESSION['question_count'] = 0;
+    ?>
+
     <div class="container">
     <h1 class="title">KnowIt All</h1>
     <p class="paragraph">A Quiz website for basic knowledge.</p>
     <button class="button" onclick="location.href='quiz.php'">Start Quiz</button>
+    <?php
+    $mysqli = new mysqli("localhost","hr4you","hr4you","hr4you_praktikum");
+    $sql = "SELECT * FROM categories ";
+    $result = $mysqli->query($sql);
+    if (!$result) {
+        echo "Error: " . $mysqli->error;
+        exit();
+    }
+    $rows = $result->fetch_all(MYSQLI_ASSOC);
+  ?>
+    <div class="buttons-wrapper">
+      <form action="/quiz.php" method="post">
+        <?php
+          foreach ($rows as $category){
+              echo '<button type="submit" class="button" name="submitCategory" value="'.$category['id'].'">'.$category['name'].'</button>';
+          }
+        ?>
+      </form>
     </div>
+</div>
   </body>
 </html>
